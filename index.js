a72586234ca10564b8c62664611366adbe83dc76
n-francky <fngabo@simactriangle.nl>
2021-11-17 23:26:46 +0100

    Clean and add create a object

--- index.js
+++ index.js
@@ -1,68 +1,44 @@
-const express = require('express');
-const axios = require('axios');
-const cheerio = require('cheerio');
-const PORT = process.env.PORT || 3000;
+const axios = require("axios");
+const cheerio = require("cheerio");
 
-const app = express();
+const _ = [];
 
+const years = ["2021"];
+const runApplication = async () => {
+  for await (yearPublished of years) {
+    const dataProcessors = await getDescriptions(yearPublished);
 
-const articles = [];
-const amplify = [];
-const newsPapers = [
-    {
-        name: "The Guardian",
-        url:"https://www.theguardian.com/environment/climate-crisis"
-    },
+    console.log("dataProcessors: ", dataProcessors);
+  }
+};
 
-    {
-        name: "The Times",
-        url:"https://www.thetimes.co.uk/environment/climate-change"
-    },
+const getDescriptions = async (yearPublished) => {
+  try {
+    const returnData = await axios.get(
+      `https://aws.amazon.com/about-aws/whats-new/${yearPublished}/`
+    );
+    const $ = cheerio.load(returnData.data);
 
-    {
-        name: "The Telegraph",
-        url:"https://www.telegraph.co.uk/climate-change"
-    }
-];
+    const listItems = $('[class="directory-list whats-new-detail"] li');
 
+    listItems.each((idx, element) => {
+      const url = $(element).find("a").attr("href");
+      const datePosted = $(element).find(".date").text();
 
-newsPapers.forEach(news =>{
-    axios.get(news.url)
-        .then(response => {
-            const html = response.data;
-            const $ = cheerio.load(html);
-            $('a:contains("climate")',html).each(function(){
-                const title = $(this).text();;
-                const url = $(this).attr('href');
-                articles.push({title, url, source: news.url});
-            })
-        }).catch(err=>console.log(err))
-})
+      _.push({
+        url,
+        datePosted,
+      });
+    });
 
-app.get('/', (req, res) => {
-    res.json('Hello world')
-})
+    return _;
 
-app.get('/news',  async (req, res) => {
-    res.json(articles);
-});
+    
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+runApplication();
 
 
-app.get('/amplify', (req, res) =>{
-    // axios.get('https://amplify.com/news')
-    axios.get('https://aws.amazon.com/new/')
-        .then(response =>{
-            // res.json(response.data);
-            const html = response.data
-            const $ = cheerio.load(html);
-            $('a',html).each(function(){
-                const title = $(this).text();
-                const url = $(this).attr('href');
-                amplify.push({title, url});
-            })
-            res.json(amplify)
-        })
-        .catch(err =>console.log(err))
-})
-
-app.listen(PORT, () => {console.log(`Server runing on port ${PORT}`)});
