5d24df8c295db7b4adc12635d86f8c94de304a5e
n-francky <fngabo@simactriangle.nl>
2021-11-18 17:40:01 +0100

    Clean the code and add more dynamc entries

--- index.js
+++ index.js
@@ -1,37 +1,42 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-const _ = [];
-const currentYear = new Date();
-console.log(currentYear.getFullYear())
-const years = ["2021"];
+const todayPeriod = new Date();
+
 const runApplication = async () => {
-    const dataProcessors = await getDescriptions(currentYear.getFullYear());
-    console.log("dataProcessors: ", dataProcessors);
+  const dataProcessors = await getDescriptions(todayPeriod);
+  console.log("dataProcessors: ", dataProcessors);
 };
 
-const getDescriptions = async (yearPublished) => {
+/**
+ * 
+ * @param {date} todayPeriod - The period of the article was published 
+ * @returns 
+ */
+const getDescriptions = async (todayPeriod) => {
+  const _ = [];
   try {
     const returnData = await axios.get(
-      `https://aws.amazon.com/about-aws/whats-new/${yearPublished}/`
+      `https://aws.amazon.com/about-aws/whats-new/${todayPeriod.getFullYear()}/`
     );
     const $ = cheerio.load(returnData.data);
 
-    const listItems = $('[class="directory-list whats-new-detail"] li');
+    const listOfPosts = $('[class="directory-list whats-new-detail"] li');
 
-    listItems.each((idx, element) => {
-      const title = $(element).find("a").text().split('Read More')[0];
-      const url = $(element).find("a").attr("href");
-      const datePosted = $(element).find(".date").text().trim().split(":")[1];
+    listOfPosts.each((idx, element) => {
+      const wantedDate = new Date( $(element).find(".date").text().trim().split(":")[1]);
+      
+      if(wantedDate.getMonth()!= todayPeriod.getMonth()) return;
 
       _.push({
-        title,
-        url,
-        datePosted,
+        title: $(element).find("a").text().split("Read More")[0],
+        url: $(element).find("a").attr("href"),
+        wantedDate,
       });
     });
 
     return _;
+  
   } catch (error) {
     console.log(error);
   }
